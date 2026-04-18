219a7d26275a2ac7faec9214d130727a4a1e29c9
Marc <malbareda@ies-sabadell.cat>
2026-04-18 16:41:32 +0200

    aaa

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/repocursdireccio/',  // ← nom del teu repo
+  base: '/repoperquesfamilies/',  // ← nom del teu repo
 })
